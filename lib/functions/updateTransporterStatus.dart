ebe87cff07dbf43d69272dbd2b3f961d5c6ff4a8
Yashaswi Anand <yash.kwr17@gmail.com>
2021-10-07 13:45:06 +0530

    LIVE-68: Error in dashboard

--- lib/functions/updateTransporterStatus.dart
+++ lib/functions/updateTransporterStatus.dart
@@ -2,10 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:liveasy_admin/functions/accessingurl.dart' as ac;
 
-
-
 Future<int?> updateTransporterStatus({required Map<String, dynamic> detailsForUpdate, String? userId}) async{
-
 
   List<String> li = await Future.wait<String>([
     ac.gettransporterurl(),
